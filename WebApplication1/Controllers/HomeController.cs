30e91e66378f0d2da2c7066f284908245711dc80
dark <darkmit@list.ru>
2019-03-06 16:25:14 +0200

    project_05_03

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -1,19 +1,28 @@
-﻿using System;
+﻿using DataLayer;
+using DataLayer.Entityes;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
 using WebApplication1.Models;
 
 namespace WebApplication1.Controllers
 {
     public class HomeController : Controller
     {
+        private EFDBContext _context;
+        public HomeController(EFDBContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
-            HelloModel _model = new HelloModel() { HelloMessage = "Hi, Serge" };
-            return View(_model);
+            //HelloModel _model = new HelloModel() { HelloMessage = "Hi, Serge" };
+            //return View(_model);
+            List<Directory> _dirs = _context.Directory.Include(x=>x.Materials).ToList();
+            return View(_dirs);
         }
 
         public IActionResult Privacy()
